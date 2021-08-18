88e1926f49a2a2444dda51f4d5b44bbd9c5f89d1
seongyeji <098dpwl@likelion.org>
2021-08-18 23:07:49 +0900

    완성본

---
+++ crud/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+
+    def summary(self):
+        return self.body[:100]
